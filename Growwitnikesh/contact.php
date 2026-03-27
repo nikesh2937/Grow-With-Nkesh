ee7eed0e01d412fd456b6337fa311b1708c4d2f5
Nikesh Kumar <nikesh_kumar@hcltech.com>
2026-03-27 17:45:05 +0530

    Fixing sonar suggestion

--- Growwitnikesh/contact.php
+++ Growwitnikesh/contact.php
@@ -1,20 +1,19 @@
-<?php 
-$errors = '';
+<?php $errors = '';
 $myemail = 'yourname@website.com';//<-----Put Your email address here.
 
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
 
 if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
@@ -44,5 +43,4 @@ if( empty($errors)) {
 
 	header('Location: thank-you.html');
 
-}
-?>
+};?>
